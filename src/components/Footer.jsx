e430086f3b59e77bbbe298a5cf8677ec529b10b0
Razwanul <mdrazwanulislam014@gmail.com>
2026-09-19 12:55:26 +0600

    give source code link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="footer">
       <span>MovieExplorer</span>
       <span>© 2026 MovieExplorer. Data from TVMaze.</span>
-      <a href="https://github.com/" target="_blank" rel="noreferrer">GitHub</a>
+      <a href="https://github.com/Razwanul30/foundation-program-assignment-2" target="_blank" rel="noreferrer">GitHub</a>
     </footer>
   );
 }
