3772637eb44eb5ef88d046872d94128e245801ee
Razwanul <mdrazwanulislam014@gmail.com>
2026-09-19 12:39:09 +0600

    add Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <span>MovieExplorer</span>
+      <span>© 2026 MovieExplorer. Data from TVMaze.</span>
+      <a href="https://github.com/" target="_blank" rel="noreferrer">GitHub</a>
+    </footer>
+  );
+}
